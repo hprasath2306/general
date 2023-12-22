7379b04ffef0a3ec7a3930a8028cab6685406f31
unknown <hprasathoff23066@gmail.com>
2023-12-22 09:38:32 +0530

    firstcommit

---
+++ todovercel/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from todo.models import Task
+
+def home(request):
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completedtasks = Task.objects.filter(is_completed=True)
+    context = {
+        'tasks':tasks,
+        'completed_tasks':completedtasks,
+    }
+    return render(request,'home.html',context)
